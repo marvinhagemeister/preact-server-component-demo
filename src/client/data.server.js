7d389a32562355cdc89868483820569b85c42b8d
Marvin Hagemeister <hello@marvinh.dev>
2021-03-12 13:45:36 +0100

    Fix invalid file type

--- src/client/data.server.js
+++ src/client/data.server.js
@@ -1,7 +1,17 @@
-export const ServerComponent = ({ state }: { state: Boolean }) => {
-  return { J0: state ? ["$", "h3", null, { "class": "foo", "children": "hello"}] : ["$", "p", null, { "children": "This is text coming from the server"}] }
-}
+module.exports = {
+	ServerComponent: ({ state }) => {
+		return {
+			J0: state
+				? ["$", "h3", null, { class: "foo", children: "hello" }]
+				: ["$", "p", null, { children: "This is text coming from the server" }],
+		};
+	},
 
-export const ServerComponent2  = ({ state }: { state: Boolean }) => {
-  return { J0: state ? ["$", "h3", null, { "children": "world!"}] : ["$", "h3", null, { "class": "foo", "children": "Franzbrötchen"}] }
-}
+	ServerComponent2: ({ state }) => {
+		return {
+			J0: state
+				? ["$", "h3", null, { children: "world!" }]
+				: ["$", "h3", null, { class: "foo", children: "Franzbrötchen" }],
+		};
+	},
+};
